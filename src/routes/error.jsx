f09c7e8f313bee204aa0005d537211b5dd20faee
Warling-Sebastian <150879739+Warling-Sebastian@users.noreply.github.com>
2024-06-07 00:48:40 -0400

    Update
    err

--- src/routes/error.jsx
+++ src/routes/error.jsx
@@ -5,6 +5,7 @@ function Error() {
     <div>
         <h1>Lo sentimos warling dice que esa pagina no existe.</h1>
         <p>para mas info llame al 809-943-9787</p>
+        <h3>wenoooo ponga eso bn</h3>
     </div>
   )
 }
